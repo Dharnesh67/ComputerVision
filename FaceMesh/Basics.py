283c6ff4481d12fdc38564a080552e117c207183
Dharmesh <raikwardharmesh67@gmail.com>
2025-06-27 19:47:55 +0530

    FaceMeshModule

--- FaceMesh/Basics.py
+++ FaceMesh/Basics.py
@@ -8,14 +8,15 @@ cap = cv.VideoCapture(0)
 mpFaceMesh = mp.solutions.face_mesh
 FaceMesh = mpFaceMesh.FaceMesh(
     # static_image_mode=False,
-    # max_num_faces=1,
+    max_num_faces=3,
     # refine_landmarks=True,
     # min_detection_confidence=0.5,
     # min_tracking_confidence=0.5,
 )
-mpDraw= mp.solutions.drawing_utils
+mpDraw = mp.solutions.drawing_utils
 prevtime = 0
 currenttime = 0
+DrawSpecs = mpDraw.DrawingSpec(color=(153, 200, 229), thickness=1, circle_radius=1)
 while True:
     success, img = cap.read()
     currenttime = time.time()
@@ -28,14 +29,32 @@ while True:
     results = FaceMesh.process(imgRGB)
     if results.multi_face_landmarks:
         for faceLms in results.multi_face_landmarks:
-            # print(faceLms.landmark)
+            for id, lm in enumerate(faceLms.landmark):
+                ih, iw, ic = img.shape
+                x = int(lm.x * iw)
+                y = int(lm.y * ih)
+                z = int(lm.z * ic)
+                print(f"id: {id}, x: {x}, y: {y}, z: {z}")
+
             mpDraw.draw_landmarks(
-                img, faceLms, mpFaceMesh.FACEMESH_TESSELATION,
-                mpDraw.DrawingSpec(color=(255,0, 0), thickness=1, circle_radius=1),
-                mpDraw.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1)
+                img,
+                faceLms,
+                mpFaceMesh.FACEMESH_TESSELATION,
+                # mpDraw.DrawingSpec(color=(255,0, 0), thickness=1, circle_radius=1),
+                DrawSpecs,
+                DrawSpecs,
+                DrawSpecs,
             )
     else:
-        cv.putText(img, "No Face Detected", (10, 70), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv.putText(
+            img,
+            "No Face Detected",
+            (10, 70),
+            cv.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 255),
+            2,
+        )
     cv.imshow("Image", img)
     if cv.waitKey(1) & 0xFF == ord("q"):
         break
